73a03c16fd2f87db11e4bbdfcd0208f501a9cd2b
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2021-08-04 12:31:42 +0300

    ref: refactor examples for elementAt() operator

--- src/01-filtering-to-one-result/03-element-at.ts
+++ src/01-filtering-to-one-result/03-element-at.ts
@@ -3,38 +3,46 @@
 //     defaultValue?: T
 // ): MonoTypeOperatorFunction<T>
 
-import { of, fromEvent } from 'rxjs';
-import { elementAt } from 'rxjs/operators';
+import { of, fromEvent, elementAt } from 'rxjs';
 import { run } from '../03-utils';
 
 // index exists, emit value and complete
-export function elementAtDemo1() {
+(function elementAtDemo1() {
   const source$ = of(5, 4, 7, 2, 8);
   const stream$ = source$.pipe(elementAt(2));
 
   // run(stream$);
-}
+})();
 
-// index doesn't exist or negative, emit error
-export function elementAtDemo2() {
+// index doesn't exist emit error
+(function elementAtDemo2() {
   const source$ = of(5, 4, 7, 2, 8);
   const stream$ = source$.pipe(elementAt(10));
 
   // run(stream$);
-}
+})();
+
+// index is negative - unhandled exception
+(function elementAtDemo3() {
+  const source$ = of(5, 4, 7, 2, 8);
+  // const stream$ = source$.pipe(elementAt(-10));
+
+  // run(stream$);
+})();
 
 // index doesn't exist, use default value and complete
-export function elementAtDemo3() {
+// BUT now throw unhandled exception
+(function elementAtDemo4() {
   const source$ = of(5, 4, 7, 2, 8);
   const stream$ = source$.pipe(elementAt(10, 100));
 
   // run(stream$);
-}
+})();
 
 // emit 2-nd click on runBtn button and complete
-export function elementAtDemo4() {
+(function elementAtDemo4() {
   const clicks$ = fromEvent(document.getElementById('runBtn'), 'click');
   const stream$ = clicks$.pipe(elementAt(1));
 
-  // run(stream$);
-}
+  // run(stream$, { outputMethod: 'console'});
+})();
--- src/index.ts
+++ src/index.ts
@@ -1,16 +1,11 @@
 import { runner } from './02-practice/tasks';
-import { elementAtDemo1, elementAtDemo2, elementAtDemo3, elementAtDemo4, minDemo1, minDemo2, minDemo3, maxDemo1, maxDemo2, maxDemo3, findDemo1, findDemo2, findIndexDemo1, findIndexDemo2, singleDemo1, singleDemo2, singleDemo3, singleDemo4, singleDemo5, findDemo3, ignoreElementsDemo1, ignoreElementsDemo2 } from './01-filtering-to-one-result';
+import { minDemo1, minDemo2, minDemo3, maxDemo1, maxDemo2, maxDemo3, findDemo1, findDemo2, findIndexDemo1, findIndexDemo2, singleDemo1, singleDemo2, singleDemo3, singleDemo4, singleDemo5, findDemo3, ignoreElementsDemo1, ignoreElementsDemo2 } from './01-filtering-to-one-result';
 console.log(`Hi, Rxjs`);
 
 
 // ************************************************
 // *                        Filtering to one result
 // ************************************************
-
-elementAtDemo1();
-elementAtDemo2();
-elementAtDemo3();
-elementAtDemo4();
 
 minDemo1();
 minDemo2();
