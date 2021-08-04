dc0e0e00d63362a084a2c36af4ca33012ec0c6e1
Vitaliy Zhyrytskyy <vitaliy_zhyrytskyy@epam.com>
2021-08-04 12:04:00 +0300

    feat: add examples for the first() operator

--- src/01-filtering-to-one-result/01-first.ts
+++ src/01-filtering-to-one-result/01-first.ts
@@ -3,35 +3,63 @@
 //     defaultValue?: D
 // ): OperatorFunction<T, T | D>
 
-import { of } from 'rxjs';
-import { first } from 'rxjs/operators';
+// One import for creational and pipable operators since v.7.2.0
+import { of, first } from 'rxjs';
 import { run } from '../03-utils';
 
 // emit only the first value
-export function firstDemo1() {
+(function firstDemo1() {
   const source$ = of(0, 1, 2, 3, 4, 5);
 
   const stream$ = source$.pipe(first());
   // run(stream$);
-}
+})();
 
 // first + predicate function
-export function firstDemo2() {
+(function firstDemo2() {
   const $source = of(1, 3, 5, 10);
   const stream$ = $source.pipe(first(n => n % 2 === 0));
   // run(stream$);
-}
+})();
 
 // no value causes an error
-export function firstDemo3() {
+(function firstDemo3() {
   const source$ = of(1, 2, 3, 4, 5);
   const stream$ = source$.pipe(first(n => n > 250));
   // run(stream$);
-}
+})();
 
 // if no value, use default
-export function firstDemo4() {
+(function firstDemo4() {
   const source$ = of(1, 2, 3, 4, 5);
   const stream$ = source$.pipe(first(n => n > 250, 1000));
   // run(stream$);
-}
+})();
+
+// Empty stream, no predicate, use default
+(function firstDemo5() {
+  const source$ = of();
+  const stream$ = source$.pipe(first(null, 1000));
+  // run(stream$);
+})();
+
+// predicate is a type guard, no default value, value doesn't exist => EmptyError
+(function firstDemo6() {
+  const source$ = of(1, 2, 3, 4, 5);
+  const stream$ = source$.pipe(first((n: number | string): n is string => typeof n === 'string'));
+  // run(stream$);
+})();
+
+// predicate is a type guard + default value, value doesn't exist
+(function firstDemo7() {
+  const source$ = of(1, 2, 3, 4, 5);
+  const stream$ = source$.pipe(first((n: number | string): n is string => typeof n === 'string', 1000));
+  run(stream$);
+})();
+
+// predicate is a type guard + default value, value exists
+(function firstDemo8() {
+  const source$ = of(1, 2, 3, 4, 5, 'Hello');
+  const stream$ = source$.pipe(first((n: number | string): n is string => typeof n === 'string', 1000));
+  run(stream$);
+})();
